4fcd5c379e804179223cf21f5a99081b09666dc1
Himantha-alt <jayashanhimantha@gmail.com>
2023-12-14 21:33:41 +0530

    Backend Sample

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from './pages/Home';
 import Join from './pages/JoinUs';
+import Driver from './pages/Driver';
+import RepairCenter from './pages/RepairCenter';
 import Services from './pages/Services';
 import About from './pages/About';
 import Pricing from './pages/Pricing';
@@ -13,7 +15,10 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/join" element={<Join />} />
+        <Route path="/join/*" element={<Join />}>
+          <Route path="driver" element={<Driver />} />
+          <Route path="repairCenter" element={<RepairCenter />} />
+        </Route>
         <Route path="/services" element={<Services />} />
         <Route path="/pricing" element={<Pricing />} />
         <Route path="/about" element={<About />} />
--- client/src/pages/Pricing.js
+++ client/src/pages/Pricing.js
@@ -4,7 +4,7 @@ function Pricing() {
   return (
     <div className="contact-page">
       <h1>Welcome to my website!</h1>
-      <p>This is a simple example of a contact page.</p>
+      <p>This is a simple example of a pricing page.</p>
     </div>
   );
 }
