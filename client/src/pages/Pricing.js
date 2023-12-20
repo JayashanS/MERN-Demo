b9811bed0068cdb5dce842503386f1d87d8a4b62
Himantha-alt <jayashanhimantha@gmail.com>
2023-12-20 21:35:32 +0530

    Updated

--- client/src/pages/Pricing.js
+++ client/src/pages/Pricing.js
@@ -1,12 +1,62 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import mapboxgl from 'mapbox-gl';
 
-function Pricing() {
-  return (
-    <div className="contact-page">
-      <h1>Welcome to my website!</h1>
-      <p>This is a simple example of a pricing page.</p>
-    </div>
-  );
-}
+import 'mapbox-gl/dist/mapbox-gl.css';
+
+const Pricing = () => {
+  useEffect(() => {
+    mapboxgl.accessToken = 'pk.eyJ1IjoiaGltYW50aGExMTY4MSIsImEiOiJjbHE2NTZtenUwcGw0MmpsbzBxNGFhY2xhIn0.byhwRJA8e5IAqtNL_-MYqQ'; // Replace with your Mapbox access token
+
+    const map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/mapbox/light-v11',
+      center: [-96, 37.8],
+      zoom: 3,
+    });
+
+    const geojson = {
+      type: 'FeatureCollection',
+      features: [
+        {
+          type: 'Feature',
+          geometry: {
+            type: 'Point',
+            coordinates: [-77.032, 38.913],
+          },
+          properties: {
+            title: 'Mapbox',
+            description: 'Washington, D.C.',
+          },
+        },
+        {
+          type: 'Feature',
+          geometry: {
+            type: 'Point',
+            coordinates: [-122.414, 37.776],
+          },
+          properties: {
+            title: 'Mapbox',
+            description: 'San Francisco, California',
+          },
+        },
+      ],
+    };
+
+    // add markers to map
+    geojson.features.forEach((feature) => {
+      // create a HTML element for each feature
+      const el = document.createElement('div');
+      el.className = 'marker';
+
+      // make a marker for each feature and add to the map
+      new mapboxgl.Marker(el).setLngLat(feature.geometry.coordinates).addTo(map);
+    });
+
+    // Clean up the map on unmount
+    return () => map.remove();
+  }, []);
+
+  return <div id="map" style={{ width: '100%', height: '100vh' }} />;
+};
 
 export default Pricing;
